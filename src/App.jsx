3215e4973c8ce449ae28c16c42cfc0398359a2d6
suresh-tech007 <sk4896323@gmail.com>
2023-12-31 18:02:53 +0530

    this is my first react project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Maincon from './componets/main';
+import Navigation from './componets/navigation';
+
+
+
+
+const App = ()=> {
+
+  return (
+    <>
+     <div>
+      <Navigation/>
+      <Maincon/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/componets/navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navigation =()=>{
+
+    return (
+        <div>
+              
+        <nav>
+        <div className="logo">
+          <img src="src/image/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>Menu</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Content</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+        </div>
+    )
+}
+export default Navigation;
